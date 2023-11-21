bfbb1ea7838725859ddfe5339e218ce8aa8ab63a
Antonio Jiménez <marshday5@gmail.com>
2023-11-21 12:42:18 -0600

    preparing the api index, ando adding the configurations

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const port = process.env.PORT || 9222;
 app.set("view engine", "pug");
 app.set("views", "./views");
 
+app.use(express.static("public"));
+app.use("/styles", express.static("public/styles", { "extensions": ["css"] }));
+
 app.get("/", (req, res) => {
-    res.render("./index/index")
+    res.render("index/index")
 }); 
 
 app.listen(port, () => console.log(`Ya estoy funcionando master en http://localhost:${port}`));
