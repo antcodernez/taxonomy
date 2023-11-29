5372fc098593dbd03d14e8dd59c352de762a6460
Antonio Jiménez <marshday5@gmail.com>
2023-11-29 15:03:32 -0600

    hot fix

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ const app = express();
 const port = process.env.PORT || 9222;
 
 //definiendo morgan para inspeccionar el server
-app.set(morgan("tiny"))
-
 app.set("view engine", "pug");
-app.set("views", "./views");
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
-app.use(express.static("public"));
-app.use("/styles", express.static("public/styles", { "extensions": ["css"] }));
+app.use(morgan("tiny"));
+
+// app.use(express.static("public"));
+// app.use("/styles", express.static("public/styles", { "extensions": ["css"] }));
 
 app.get("/", (req, res) => {
-    res.render("index/index")
+    res.send("`<h1>Hola mundo</h1>`")
 }); 
 
 app.listen(port, () => console.log(`Ya estoy funcionando master en http://localhost:${port}`));
