14af84f93c6b28bd6d94551593481cee8d52cb79
Angel234LM <angelmaldonado9275@gmail.com>
2023-11-29 14:46:40 -0600

    implementando morgan para inspexionar las peticiones de la api

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const morgan =require("morgan");
 const app = express();
 const port = process.env.PORT || 9222;
+
+//definiendo morgan para inspeccionar el server
+app.set(morgan("tiny"))
 
 app.set("view engine", "pug");
 app.set("views", "./views");
