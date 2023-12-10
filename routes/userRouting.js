49864df215e52869bdbf07b23bc47b53b7795799
Antonio Jiménez <marshday5@gmail.com>
2023-12-10 17:03:29 -0600

    hot fix

--- routes/userRouting.js
+++ routes/userRouting.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
    res.render('auth/login', {
-            pagina: 'Iniciar Sesión',
+            page: 'Iniciar Sesión',
             // csrfToken: req.csrfToken()
         })
 });
