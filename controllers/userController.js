4be638016797430b3e57b091b8c7ceef89564a49
antcodernez <marshday5@gmail.com>
2023-12-11 19:15:13 -0600

    hot fix merge

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-const { check, validationResult }= require ("express-validator");
-const { generatedID, jwtToken } =require("../lib/tokens.js");
-const bcrypt = require("bcrypt");
-const dotenv = require("dotenv");
-
-const formLogin = (req, res) => {
-    res.render('auth/login.pug', {
-        page: 'Inicio de sesion',
-        isLogged: false,
-        cssFile: "/public/css/lappLogin.css"
-      })
-    }
-
-    const formRegister = (request, response) => {
-        response.render("auth/register.pug", { page: "Registro", cssFile: "/public/css/appRegister.css" });
-      };
-
-module.exports = {formLogin, formRegister}
-=======
 const formRegister = async( req, res) => {
     res.render('auth/register', {
             page: 'Crear cuenta',
@@ -44,4 +24,3 @@ module.exports = {
     formRegister,
     registerUser
 };
->>>>>>> 6c3f8e99264cf9b860a970f4e7d5944044c08cb5
